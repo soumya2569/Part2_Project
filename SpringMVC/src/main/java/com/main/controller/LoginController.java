0f972138d6ca62cdad337751a0aade25b4504153
Shanthi-Swaroop-P <62928269+Shanthi-Swaroop-P@users.noreply.github.com>
2020-06-08 17:49:39 +0530

    Second Service Commit

--- SpringMVC/src/main/java/com/main/controller/LoginController.java
+++ SpringMVC/src/main/java/com/main/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.main.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.main.entity.User;
@@ -15,11 +16,19 @@ public class LoginController {
 	@Autowired
 	UserService userser;
 	
-	@GetMapping("/login")
+	@PostMapping("/login")
 	public ResponseMessage LoginUser(@RequestBody User user) {
 		ResponseMessage response=new ResponseMessage();
 		response.setMessageCode("MSG1");
 		response.setMessageDesc(userser.validateUser(user));
 		return response;
 	}
+	
+	@GetMapping("/userdetails")
+	public String UserDetails() {
+		String res="{Username : admin,"+"emailid :admin@infy.com,"+"contactnumber:9848022338";
+		return res;
+	}
+	
+	
 }
