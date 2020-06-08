e5a808662caf2090d358ae2f97b381f30ebb5522
Shanthi-Swaroop-P <62928269+Shanthi-Swaroop-P@users.noreply.github.com>
2020-06-08 13:07:33 +0530

    Add files via upload

---
+++ SpringMVC/src/main/java/com/main/controller/LoginController.java
@@ -0,0 +1,25 @@
+package com.main.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.main.entity.User;
+import com.main.entity.ResponseMessage;
+import com.main.service.UserService;
+
+
+@RestController
+public class LoginController {
+	
+	@Autowired
+	UserService userser;
+	
+	@GetMapping("/login")
+	public ResponseMessage LoginUser(@RequestBody User user) {
+		ResponseMessage response=new ResponseMessage();
+		response.setMessageCode("MSG1");
+		response.setMessageDesc(userser.validateUser(user));
+		return response;
+	}
+}
