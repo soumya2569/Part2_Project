b19a4bb4cc1972dc9c0dc88bf6dc15cd6cb5db01
Ashay <ashaymurceilago@gmail.com>
2020-06-08 15:24:30 +0530

    Added view to the main project

--- SpringMVC/src/main/java/com/main/controller/LoginController.java
+++ SpringMVC/src/main/java/com/main/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.main.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.main.entity.User;
@@ -15,7 +16,7 @@ public class LoginController {
 	@Autowired
 	UserService userser;
 	
-	@GetMapping("/login")
+	@PostMapping("/login")
 	public ResponseMessage LoginUser(@RequestBody User user) {
 		ResponseMessage response=new ResponseMessage();
 		response.setMessageCode("MSG1");
